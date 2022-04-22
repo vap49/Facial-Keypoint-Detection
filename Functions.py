605d505ce91fec8771c85bfa84b7267e62798a04
dilipnjit <dc575@njit.edu>
2022-04-22 13:50:30 -0400

    created tanh and sigmoid functions

---
+++ Functions.py
@@ -0,0 +1,24 @@
+from Activation import Activation
+from jax.numpy import numpy as np
+class Tanh(Activation):
+  def __init__(self):
+    
+    def tanh(x):
+      return np.tanh(x)
+    
+    def tanh_prime(x):
+      return 1 - np.tanh(x) ** 2
+    
+    super().__init__(tanh, tanh_prime)
+
+class Sigmoid(Activation):
+  def __init__(self):
+    def sigmoid(x):
+      return 1 / (1 + np.exp(-x))
+    
+    def sigmoid_prime(x):
+      s = sigmoid(x)
+
+      return s * (1-s)
+
+    super().__init__(sigmoid, sigmoid_prime)

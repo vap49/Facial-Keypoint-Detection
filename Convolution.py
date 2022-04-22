a9eacc45ccbc44ae360f6ed17a372873ea1cb3e6
dilipnjit <dc575@njit.edu>
2022-04-22 13:39:04 -0400

    added second conv layer implementation

--- Convolution.py
+++ Convolution.py
@@ -1,4 +1,5 @@
 from Layer import Layer
+from typing_extensions import ParamSpecArgs
 from jax.numpy import numpy as np
 from scipy import signal
 from jax._src.api import jit
@@ -48,3 +49,36 @@ class Convolutional(Layer):
     self.biases -= lr * o_grad
 
     return input_gradient
+
+class Convolutional_2(Layer):
+  def __init__(self, num_filters, filter_size):
+    self.num_filters = num_filters
+    self.filter_size = filter_size
+    self.conv_filter = np.random.randn(num_filters, filter_size, filter_size) / (filter_size * filter_size)
+
+  def image_region(self, image):
+    height, width = image.shape
+    self.image = image
+    for j in range(height - self.filter_size + 1):
+      for k in range(width - self.filter_size + 1):
+        image_patch = image [j : (j+self.filter_size), k:( k + self.filter_size)]
+        yield image_patch, j, k
+  
+  def forward(self, images):
+    height, width = images.shape
+    self.image = images
+
+    conv_out = np.zeros((height - self.filter_size + 1 , width - self.filter_size + 1, self.num_filters))
+    for image_patch , i , j in self.image_region(images):
+      conv_out[i,j] = np.sum(image_patch * self.conv_filter, axis = (1,2))
+    return conv_out
+
+  def backward(self, dL_dout, lr):
+    dL_dF_params = np.zeros(self.conv_filter.shape)
+    for image_patch, i, j in self.image_region(self.images):
+      for k in range(self.num_filters):
+        dL_dF_params[k] += image_patch * dL_dout[i,j,k]
+    
+    #update params
+    self.conv_filter -= lr * dL_dF_params
+    return dL_dF_params
